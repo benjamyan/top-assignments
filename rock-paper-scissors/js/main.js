aa05412bc96e15d03e64d47f8f551f2bf3fd5fb3
unknown <benyannella@gmail.com>
2020-01-25 07:45:05 -0500

    routine update | almost done

--- rock-paper-scissors/js/main.js
+++ rock-paper-scissors/js/main.js
@@ -1,5 +1,10 @@
-const playerSelect = (select) => {
-    return select;
+let rounds = [];
+let i = 0;
+let text = document.querySelector("h3");
+let playerNum = document.querySelector(".player h1");
+let computerNum = document.querySelector(".computer h1");
+const playerSelect = (value) => {
+    return value;
 };
 const computerPlay = () => {
     const num = Math.random();
@@ -10,27 +15,46 @@ const computerPlay = () => {
 };
 
 function compare(player, computer) {
-    let p = player, c = computer, num;
-    if (p == c) return "Draw!";
-    if (p == "rock") {
-        if (c == "paper") return "Paper beats rock!";
-        if (c == "scissors") return "Rock beats paper!";
+    let p = player, c = computer, loss, win, num, total;
+    loss = `${c} beats ${p}!`;
+    win = `${p} beats ${c}!`;
+    if (p == c) total = ["Draw!", num=0];
+    else if (p == "rock") {
+        if (c == "paper") total = [loss, num=-1];
+        if (c == "scissors") total = [win, num=1];
     } else if (p == "paper") {
-        if (c == "rock") return "Rock beats paper!";
-        if (c == "scissors") return "Scissors beat paper!";
+        if (c == "rock") total = [win, num=1];
+        if (c == "scissors") total = [loss, num=-1];
     } else {
-        if (c == "rock") return "Rock beats scissors!";
-        if (c == "paper") return "Scissors beats rock!";
+        if (c == "rock") total = [loss, num=-1];
+        if (c == "paper") total = [win, num=1];
+    }
+    return total;
+};
+function winner() {
+    if (playerNum > computerNum) {
+        text.innerText = "You win!"
+    } else {
+        text.innerText = "You lose!"
     };
+    playerNum.innerText = 0;
+    computerNum.innerText = 0;
 };
 function game(value) {
-    for(var i = 0; i < 4; i++) {
-        console.log(compare(playerSelect(value), computerPlay()));
-    };
+    let result = compare(playerSelect(value), computerPlay());
+    i++;
+    text.innerText = result[0];
+    if (result[1] == -1) 
+        playerNum.innerText++;
+    else if (result[1] == 1) 
+        computerNum.innerText++;
+    else null;
+    rounds.push(result)
 };
 
 Object.values(document.querySelectorAll("button")).forEach(function(current){
     current.addEventListener("click", function() {
-        game(current.innerText.toLowerCase());
+        if (i > 5) winner();
+        else game(current.innerText.toLowerCase());
     });
 });
