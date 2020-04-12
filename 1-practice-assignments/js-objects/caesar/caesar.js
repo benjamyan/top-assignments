20ed5079665bb4602a4930a59b680b099c493977
OnlyByDesign <benyannella@gmail.com>
2020-04-12 03:00:04 -0400

    updated main function file

--- 1-practice-assignments/js-objects/caesar/caesar.js
+++ 1-practice-assignments/js-objects/caesar/caesar.js
@@ -8,14 +8,16 @@ getIndex = (element)=> { // find the index of current letter
     if (b >= 0) return b // if character isnt a glyph
     else return a // if is a glyph or whitespace
 }
-getNewChar = (ind, num)=> { // finds new character based on index and number given
+getNew = (ind, num)=> { // finds new character based on index and number given
     function newIndex(i,n) { // callback function for return
         let c = i + n, // calc base value
             d = letters.length; // call variable for array length
-        do { // run through function recursively
-            if (c === d) c = d - c; // if equal to 26
+        do { // run function recursively
+            c === d ? c = d - c : c > d ? c = c - d : c = d + c;
+            // if statement version of trinary
+            /*if (c === d) c = d - c; // if equal to 26
             if (c > d) c = c - d; // if great than 26
-            if (c < 0) c = d + c; // if less than 0
+            if (c < 0) c = d + c; // if less than 0*/
         } while (c >= d) return c // return final value
     }
     return letters[newIndex(ind, num)] // return value from function
@@ -33,8 +35,8 @@ const caesar = function(str, num) {
     target = new Array(); // creates a new array for each call
     Array.from(str).forEach(function(current){ // convert string to array and loop through it
         let ind = getIndex(current); // selects current element in array
-        // determine either new character or glyph to push to target array
-        !isNaN(parseInt(ind)) ? target.push(getNewChar(ind, num)) : target.push(ind);
+        // determine either a new character or glyph is pushed to target array
+        !isNaN(parseInt(ind)) ? target.push(getNew(ind, num)) : target.push(ind);
     });
     return getFinal() // return final value from function
 }
